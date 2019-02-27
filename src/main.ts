04194804f1a85f6a9b61cca09b016cd7a2021c4c
Jayson San Agustin <jayson.groundgurus@gmail.com>
2019-02-27 10:11:49 +0800

    Added Security

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,19 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, catDocument);
   SwaggerModule.setup('api/docs', app, document);
+
+  app.enableCors();
+
+  app.use(helmet({
+    hidePoweredBy: {
+      setTo: 'PHP/7.0.27',
+    },
+  }));
+
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }));
 
   await app.listen(3000);
 }
